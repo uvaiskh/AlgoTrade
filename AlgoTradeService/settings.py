8e5527b329fa9ce7d2329d4c3120bd3d1f6ddbe2
uvaiskh <35721915+uvaiskh@users.noreply.github.com>
2023-08-04 00:49:41 +0530

    Update settings.py

--- AlgoTradeService/settings.py
+++ AlgoTradeService/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8wrw@+*$6@-834bw2lb37tujrqmp)%_=lcm#w#yb$v7dldgw1='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
